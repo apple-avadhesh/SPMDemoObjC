2d328744a94123971e5874fee7f7315e31ed65bc
Avadhesh <avadhesh.sharma@mobileprogrammingllc.com>
2021-07-31 04:39:18 +0530

    SPMObjC_Check

--- Sources/SPMDemoObjC/SPMDemoObjC.swift
+++ Sources/SPMDemoObjC/SPMDemoObjC.swift
@@ -1,3 +1,32 @@
-struct SPMDemoObjC {
-    var text = "Hello, World!"
+import Foundation
+
+public struct RicStruct {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func sayHello() -> String {
+        "Hello Ric!"
+    }
+}
+
+public class RicClass: NSObject {
+    @objc public var msg = "Mother has a feeling, I might be too appealing."
+    @objc public let text = "Hello Everybody!"
+    
+    
+    public init(msg: String) {
+        if msg.isEmpty {
+            self.msg = "Hello Ric!"
+        }
+    }
+  
+    public func sayHello() {
+            print(text)
+        }
+    
+    public func doSomething() {
+        print("Inside doSomething()")
+    }
 }
